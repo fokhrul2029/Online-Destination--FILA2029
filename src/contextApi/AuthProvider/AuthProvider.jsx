8c678f7e0a2598a0413e4e534d4e16250de5543f
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-04-13 14:28:55 +0600

    added some function on AuthContext

--- src/contextApi/AuthProvider/AuthProvider.jsx
+++ src/contextApi/AuthProvider/AuthProvider.jsx
@@ -1,15 +1,42 @@
 /* eslint-disable react/prop-types */
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { createContext } from "react";
+import auth from "../../firebase/firebase.confiq";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
 
-    
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
+  const logOut = () => {
+    return signOut(auth);
+  };
 
-  const authInfo = "Hello World";
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    setLoader(false);
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = { user, createUser, signIn, logOut, loader };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
