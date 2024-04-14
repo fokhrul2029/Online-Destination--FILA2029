9cb814f0ffa3e012781deb2750e14a9a57894be5
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-04-14 15:12:36 +0600

    update card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 /* eslint-disable react/prop-types */
 function Card({ data }) {
   const navigate = useNavigate();
-  const { id, image, estate_title, description } = data;
+  const { id, image, estate_title, description, location, status } =
+    data;
 
   const handleDetailsButton = (id) => {
     navigate(`estateDetails/${id}`);
@@ -16,14 +17,22 @@ function Card({ data }) {
       </figure>
       <div className="card-body px-5">
         {" "}
-        <h2 className="card-title">{estate_title}</h2>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
+        <h2 className="card-title font-bold">{estate_title}</h2>
+        <p className="text-lg font-medium text-gray-400">{description}</p> 
+        <div className="flex w-fit gap-2 items-center flex-wrap">
+          <p className="font-bold">Location: </p>
+          <p>{location}</p>
+        </div>
+        <div className="flex w-fit gap-2 items-center">
+          <p className="font-bold">Status: </p>
+          <p className="font-semibold text-green-600">{status}</p>
+        </div>
+        <div className="card-actions justify-center mt-4">
           <button
             onClick={() => handleDetailsButton(id)}
-            className="btn btn-primary"
+            className="btn btn-primary w-full"
           >
-            Buy Now
+            View Property
           </button>
         </div>
       </div>
--- src/pages/shared/Estate/Estate.jsx
+++ src/pages/shared/Estate/Estate.jsx
@@ -5,7 +5,9 @@ function Estate() {
   const data = useLoaderData();
   return (
     <div className="py-20">
-      <h1 className="text-5xl font-semibold">Estate</h1>
+      <h1 className="text-5xl font-semibold text-center">
+        <span className="px-10 border-b-4">Estate</span>
+      </h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 py-10">
         {data.map((item) => (
           <Card key={item.id} data={item} />
